964b4744738b2fa3db30da168e2a2281532a5ee9
Ali <alifradi2011@gmail.com>
2015-08-10 21:32:52 +0100

    10/08

--- CheckBills/app/src/main/java/com/sourcey/materiallogindemo/DetailsActivity.java
+++ CheckBills/app/src/main/java/com/sourcey/materiallogindemo/DetailsActivity.java
@@ -3,12 +3,16 @@ package com.sourcey.materiallogindemo;
 /**
  * Created by F_ALI on 10/08/2015.
  */
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.oguzdev.circularfloatingactionmenu.library.FloatingActionButton;
 import com.squareup.picasso.Picasso;
 
 public class DetailsActivity extends AppCompatActivity {
@@ -36,6 +40,22 @@ public class DetailsActivity extends AppCompatActivity {
         Picasso.with(DetailsActivity.this).load(mContact.getThumbnailDrawable()).into(ivProfile);
         tvName.setText(mContact.getName());
         tvPhone.setText(mContact.getNumber());
+
+
+
+        // Extract FAB
+      FloatingActionButton  fab = (FloatingActionButton) findViewById(R.id.fab);
+        // Dial contact's number.
+        // This shows the dialer with the number, allowing you to explicitly initiate the call.
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String uri = "tel:" + mContact.getNumber();
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse(uri));
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
