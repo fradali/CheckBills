8aba44e2bc6fa828bbebbc28a8498abf11245719
Ali <alifradi2011@gmail.com>
2015-08-18 00:35:31 +0100

    update for med

--- CheckBills/app/src/main/java/com/sourcey/materiallogindemo/BillsPayed.java
+++ CheckBills/app/src/main/java/com/sourcey/materiallogindemo/BillsPayed.java
@@ -1,17 +1,157 @@
 package com.sourcey.materiallogindemo;
 
-import android.support.v4.app.Fragment;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.params.BasicHttpParams;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  * Created by F_ALI on 03/08/2015.
  */
 public class BillsPayed extends Fragment {
+
+    String myJSON;
+
+    private static final String TAG_RESULTS="result";
+    private static final String TAG_ID = "id";
+    private static final String TAG_NAME = "name";
+    private static final String TAG_DATEPY ="datepayment";
+
+    JSONArray bills = null;
+
+    ArrayList<HashMap<String, String>> billsList;
+
+    ListView list;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.billspayed, container, false);
+        View rootView = inflater.inflate(R.layout.billspayed, container, false);
+        list = (ListView) rootView.findViewById(R.id.listView);
+        billsList = new ArrayList<HashMap<String,String>>();
+        getData();
+        return rootView;
     }
+
+
+    public void showList(){
+        try {
+            JSONObject jsonObj = new JSONObject(myJSON);
+            bills = jsonObj.getJSONArray(TAG_RESULTS);
+
+            for(int i=0;i<bills.length();i++){
+                JSONObject c = bills.getJSONObject(i);
+                String id = c.getString(TAG_ID);
+                String name = c.getString(TAG_NAME);
+                String datepayement = c.getString(TAG_DATEPY);
+
+                HashMap<String,String> bill = new HashMap<String,String>();
+
+                bill.put(TAG_ID,id);
+                bill.put(TAG_NAME,name);
+                bill.put(TAG_DATEPY ,datepayement);
+
+                billsList.add(bill);
+            }
+
+            ListAdapter adapter = new SimpleAdapter(
+                   getActivity(), billsList, R.layout.list_item,
+                    new String[]{TAG_ID,TAG_NAME,TAG_DATEPY},
+                    new int[]{R.id.id, R.id.name, R.id.datepayment}
+            );
+
+            list.setAdapter(adapter);
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void getData(){
+        class GetDataJSON extends AsyncTask<String, Void, String> {
+
+            @Override
+            protected String doInBackground(String... params) {
+                DefaultHttpClient httpclient = new DefaultHttpClient(new BasicHttpParams());
+                HttpPost httppost = new HttpPost("http://192.168.200.1/get-billspayed.php");
+
+                // Depends on your web service
+                httppost.setHeader("Content-type", "application/json");
+
+                InputStream inputStream = null;
+                String result = null;
+                try {
+                    HttpResponse response = httpclient.execute(httppost);
+                    HttpEntity entity = response.getEntity();
+
+                    inputStream = entity.getContent();
+                    // json is UTF-8 by default
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, "UTF-8"), 8);
+                    StringBuilder sb = new StringBuilder();
+
+                    String line = null;
+                    while ((line = reader.readLine()) != null)
+                    {
+                        sb.append(line + "\n");
+                    }
+                    result = sb.toString();
+                } catch (Exception e) {
+                    // Oops
+                }
+                finally {
+                    try{if(inputStream != null)inputStream.close();}catch(Exception squish){}
+                }
+                return result;
+            }
+
+
+           public void onPostExecute(String result){
+                myJSON=result;
+                showList();
+            }
+        }
+        GetDataJSON g = new GetDataJSON();
+        g.execute();
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
 }
